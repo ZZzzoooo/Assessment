228ef607d0c10370b201a05d255e6e66b970d3c1
bigoxdev <bigox.dev2020@gmail.com>
2023-07-02 22:06:21 -0400

    revise the code for usage of httpclient and DI, added a offline feature

--- UndoAssessment/UndoAssessment/Services/ApiService.cs
+++ UndoAssessment/UndoAssessment/Services/ApiService.cs
@@ -2,39 +2,98 @@
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+using UndoAssessment.Models;
+using SQLite;
+using System.IO;
+using Xamarin.Essentials;
+using System.Linq;
 
 namespace UndoAssessment.Services
 {
-    public class ApiService
+    public class ApiService : IApiService
     {
+        private static readonly Lazy<HttpClient> _httpClient = new Lazy<HttpClient>(() => new HttpClient());
+        private SQLiteAsyncConnection _database;
+
+        public ApiService()
+        {
+            InitializeDatabase();
+        }
+
+        private async void InitializeDatabase()
+        {
+            string databasePath = Path.Combine(FileSystem.AppDataDirectory, "Undo.db");
+            _database = new SQLiteAsyncConnection(databasePath);
+            await CreateTableIfNotExists<ApiResponse>();
+        }
+
         private const string SuccessEndpoint = "https://malkarakundostagingpublicapi.azurewebsites.net/success";
         private const string FailEndpoint = "https://malkarakundostagingpublicapi.azurewebsites.net/fail";
 
-        public async Task<string> InvokeSuccessApi()
+        public async Task<ApiResponse> InvokeSuccessApi()
         {
-            return await InvokeApiEndpoint(SuccessEndpoint);
+            return await InvokeApiEndpoint(SuccessEndpoint, "SuccessAPI");
         }
 
-        public async Task<string> InvokeFailApi()
+        public async Task<ApiResponse> InvokeFailApi()
         {
-            return await InvokeApiEndpoint(FailEndpoint);
+            return await InvokeApiEndpoint(FailEndpoint, "FailAPI");
         }
 
-        private async Task<string> InvokeApiEndpoint(string endpoint)
+        public async Task<ApiResponse> GetStoredApiResponseAsync()
         {
-            using (var httpClient = new HttpClient())
+            var storedApiResponse = await _database.Table<ApiResponse>().FirstOrDefaultAsync();
+            return storedApiResponse;
+        }
+
+        private async Task<ApiResponse> InvokeApiEndpoint(string endpoint, string api_type)
+        {
+
+            try
             {
-                try
+                var response = await _httpClient.Value.GetAsync(endpoint).ConfigureAwait(false);
+                var content = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+                if (response.IsSuccessStatusCode)
                 {
-                    var response = await httpClient.GetAsync(endpoint);
-                    var content = await response.Content.ReadAsStringAsync();
-                    return response.IsSuccessStatusCode ? content : throw new Exception(content);
+                    var apiResponse = new ApiResponse { };
+                    if (api_type == "SuccessAPI")
+                    {
+                        apiResponse.ApiInvokeSuccessMessage = content;
+                    }
+                    else if (api_type == "FailAPI")
+                    {
+                        apiResponse.ApiInvokeFailureMessage = content;
+                    }
+                    await _database.InsertAsync(apiResponse).ConfigureAwait(false);
+                    return apiResponse;
                 }
-                catch (Exception ex)
+                else
                 {
-                    throw new Exception(ex.Message);
+                    throw new Exception(content);
                 }
             }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        private async Task CreateTableIfNotExists<T>() where T : new()
+        {
+            string tableName = typeof(T).Name;
+            bool tableExists = await TableExists(tableName).ConfigureAwait(false);
+
+            if (!tableExists)
+            {
+                await _database.CreateTableAsync<T>().ConfigureAwait(false);
+            }
+        }
+
+        private async Task<bool> TableExists(string tableName)
+        {
+            string query = $"SELECT 1 FROM sqlite_master WHERE type='table' AND name='{tableName}' LIMIT 1";
+            var result = await _database.ExecuteScalarAsync<int>(query).ConfigureAwait(false);
+            return result == 1;
         }
     }
 }
--- UndoAssessment/UndoAssessment/ViewModels/ApiInvokePageViewModel.cs
+++ UndoAssessment/UndoAssessment/ViewModels/ApiInvokePageViewModel.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Net.NetworkInformation;
 using System.Threading.Tasks;
 using UndoAssessment.Models;
 using UndoAssessment.Services;
 using UndoAssessment.Views;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace UndoAssessment.ViewModels
@@ -16,7 +19,7 @@ namespace UndoAssessment.ViewModels
 
         public string userInfoText;
 
-        private readonly ApiService apiService;
+        private ApiService apiService;
 
         public string UserInfoText
         {
@@ -43,8 +46,23 @@ namespace UndoAssessment.ViewModels
         {
             try
             {
-                string response = await apiService.InvokeSuccessApi();
-                await Application.Current.MainPage.DisplayAlert("Success", response, "OK");
+                ApiResponse _response;
+
+                if (IsInternetActive())
+                {
+                    // If the network is connected, then get the API response by calling the API
+                    _response = await apiService.InvokeSuccessApi();
+                }
+                else
+                {
+                    // If the network is off, then retrieve the API response from the DB
+                    _response = await apiService.GetStoredApiResponseAsync();
+                }
+
+                if (_response != null)
+                {
+                    await Application.Current.MainPage.DisplayAlert("Success", _response.ApiInvokeSuccessMessage, "OK");
+                }
             }
             catch (Exception ex)
             {
@@ -56,8 +74,23 @@ namespace UndoAssessment.ViewModels
         {
             try
             {
-                string response = await apiService.InvokeFailApi();
-                await Application.Current.MainPage.DisplayAlert("Error", response, "OK");
+                ApiResponse _response;
+
+                if (IsInternetActive())
+                {
+                    // If the network is connected, then get the API response by calling the API
+                    _response = await apiService.InvokeFailApi();
+                }
+                else
+                {
+                    // If the network is off, then retrieve the API response from the DB
+                    _response = await apiService.GetStoredApiResponseAsync();
+                }
+
+                if (_response != null)
+                {
+                    await Application.Current.MainPage.DisplayAlert("Error", _response.ApiInvokeFailureMessage, "OK");
+                }
             }
             catch (Exception ex)
             {
@@ -65,10 +98,22 @@ namespace UndoAssessment.ViewModels
             }
         }
 
+        // This is for furture implementation when need to see the stored variables.
+        private async Task<ApiResponse> LoadStoredApiResponseAsync()
+        {
+            return await apiService.GetStoredApiResponseAsync();
+        }
+
         private async void ShowUserInfoForm()
         {
             // Implement navigation to the user info form page
             await Shell.Current.GoToAsync(nameof(UserInfoFormPage));
+        }
+
+        private bool IsInternetActive()
+        {
+            // We have this own function to expand its functionality in the furture
+            return Connectivity.NetworkAccess == NetworkAccess.Internet;
         }
     }
 }
