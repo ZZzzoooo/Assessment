e01641e7bf72851cbeed18cc41959de3cce47d97
Mark Iliiev <ilievmarik@gmail.com>
2023-07-09 19:47:20 +0300

    Added task page to shell

---
+++ UndoAssessment/UndoAssessment/Views/TaskPage.xaml.cs
@@ -0,0 +1,12 @@
+using Xamarin.Forms;
+
+namespace UndoAssessment.Views
+{
+    public partial class TaskPage : ContentPage
+    {
+        public TaskPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
