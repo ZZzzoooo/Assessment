54bbc0f394c8ce58a315d39809f51d2bea4b66b0
Mark Iliiev <ilievmarik@gmail.com>
2023-07-09 20:29:00 +0300

    Added request builder and api service impl

--- UndoAssessment/UndoAssessment/Extensions/HttpResponseMessageExtensions.cs
+++ UndoAssessment/UndoAssessment/Extensions/HttpResponseMessageExtensions.cs
@@ -5,6 +5,9 @@ namespace UndoAssessment.Extensions
 {
     public static class HttpResponseMessageExtensions
     {
+        public static async Task<T> ReadContentAsJsonAsync<T>(this Task<HttpResponseMessage> taskMessage)
+            => await (await taskMessage).ReadContentAsJsonAsync<T>();
+
         public static async Task<T> ReadContentAsJsonAsync<T>(this HttpResponseMessage message)
             => (await message.ReadAsStringAsync()).ParseAsJson<T>();
 
--- UndoAssessment/UndoAssessment/Services/Api/TaskApiService.cs
+++ UndoAssessment/UndoAssessment/Services/Api/TaskApiService.cs
@@ -1,26 +1,35 @@
 using System.Net.Http;
 using System.Threading.Tasks;
+using UndoAssessment.Extensions;
 using UndoAssessment.Models;
 
 namespace UndoAssessment.Services.Api
 {
     public class TaskApiService : IApiService
     {
+        private const string ApiEndpoint = "https://malkarakundostagingpublicapi.azurewebsites.net";
+        
         private readonly HttpClient _client;
 
-        public TaskApiService(HttpClient client)
+        public TaskApiService()
         {
-            _client = client;
+            _client = new HttpClient();
         }
         
         public Task<ApiResponse> SuccessAsync()
-        {
-            
-        }
-
+            => _client
+                .CreateRequest()
+                .ByResource(ApiEndpoint, "/success")
+                .WithMethod(HttpMethod.Get)
+                .MakeRequestAsync()
+                .ReadContentAsJsonAsync<ApiResponse>();
+        
         public Task<ApiResponse> ErrorAsync()
-        {
-            
-        }
+            => _client
+                .CreateRequest()
+                .ByResource(ApiEndpoint, "/fail")
+                .WithMethod(HttpMethod.Get)
+                .MakeRequestAsync()
+                .ReadContentAsJsonAsync<ApiResponse>();
     }
 }
