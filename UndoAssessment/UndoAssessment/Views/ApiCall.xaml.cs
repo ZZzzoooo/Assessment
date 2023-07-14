6ebfd190a9253fcba6ed3714a1db6abd02bfe070
OskarLis <oskar.lis.0105@hotmail.com>
2023-07-14 02:22:36 -0400

    Add two buttons of invoking endpoints on given API

--- UndoAssessment/UndoAssessment/Views/ApiCall.xaml.cs
+++ UndoAssessment/UndoAssessment/Views/ApiCall.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,6 +16,25 @@ namespace UndoAssessment.Views
         public ApiCall()
         {
             InitializeComponent();
+        }
+        private async void onSuccessBtnClicked(object sender, System.EventArgs e)
+        {
+            await CallApi("https://malkarakundostagingpublicapi.azurewebsites.net/success");
+        }
+
+        private async void onFailBtnClicked(object sender, System.EventArgs e)
+        {
+            await CallApi("https://malkarakundostagingpublicapi.azurewebsites.net/fail");
+        }
+        private async Task CallApi(string url)
+        {
+            var client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync(url);
+
+            string alertTitle = response.IsSuccessStatusCode ? "Success" : "Error";
+            string alertMessage = response.IsSuccessStatusCode ? "Call succeeded" : "Call failed";
+
+            await DisplayAlert(alertTitle, alertMessage, "OK");
         }
     }
 }
