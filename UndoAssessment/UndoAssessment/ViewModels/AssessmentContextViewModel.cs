5f6aeac4bfba3ce6cadc6dfd98733c2ad3e877fb
Kubilay Kenan <kubilay.kenan@tupras.com.tr>
2023-09-20 15:22:59 +0300

    assessment 1

---
+++ UndoAssessment/UndoAssessment/ViewModels/AssessmentContextViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UndoAssessment.ViewModels
+{
+    public static class AssessmentContextViewModel
+    {
+        private static AssessmentViewModel _assessmentViewModel = new AssessmentViewModel();
+        public static AssessmentViewModel AssessmentViewModel
+        {
+            get
+            {
+                return _assessmentViewModel;
+            }
+        }
+    }
+}
