bf6ab4d48c5db9380ed166d7c1fcd2d01dfb5ec8
kostadinmikic <kostadinmikic@gmail.com>
2023-09-25 22:07:57 +0200

    Check for succesful build

---
+++ UndoAssessment/UndoAssessment/ViewModels/AssessmentViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.CommunityToolkit.ObjectModel;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace UndoAssessment.ViewModels
+{
+    public class AssessmentViewModel : BaseViewModel
+    {
+        public ICommand SuccessCommand { get; }
+        public ICommand FailCommand { get; }
+
+        public AssessmentViewModel()
+        {
+            Title = "Assessment";
+            SuccessCommand = new AsyncCommand(async () => await CallSucc());
+            FailCommand = new AsyncCommand(async () => await CallFail());
+        }
+
+
+        public async Task<string> CallSucc()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<string> CallFail()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UndoAssessment/UndoAssessment/Views/AssessmentPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel;
+using UndoAssessment.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace UndoAssessment.Views
+{
+    public partial class AssessmentPage : ContentPage
+    {
+        public AssessmentPage()
+        {
+            InitializeComponent();
+            this.BindingContext = new AssessmentViewModel();
+        }
+    }
+}
